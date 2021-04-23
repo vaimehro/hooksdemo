c544dfa2b83faa33029660a717847c11a00c2fb5
Vaibhav <Vaibhav.Mehrotra@ellucian.com>
2021-04-23 22:22:50 +0530

    hook demo code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import HookCounter from "./components/HookCounter";
+import HookCounterTwo from "./components/HookCounterTwo";
+import HookForObject from "./components/HookForObject";
+import HooksForArray from "./components/HooksForArray";
+import EffectHooksForConditionalRender from "./components/EffectHooksForConditionalRender";
+import HookMouse from "./components/HookMouse";
+import EffectHookWithApiCall from "./components/EffectHookWithApiCall";
+import HookUseContextExample from "./components/HookUseContextExample";
+import HookUserReducer from "./components/HookUserReducer";
+
+import DocTitleOne from "./components/DocTitleOne";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/*<HookCounter></HookCounter>*/}
+        {/*<HookCounterTwo></HookCounterTwo>*/}
+        {/*<HookForObject></HookForObject>*/}
+        {/*<HooksForArray></HooksForArray>*/}
+        {/*<EffectHooksForConditionalRender></EffectHooksForConditionalRender>*/}
+        {/*<HookMouse></HookMouse>*/}
+        {/*<EffectHookWithApiCall></EffectHookWithApiCall>*/}
+        {/*<HookUseContextExample></HookUseContextExample>*/}
+        {/*<HookUserReducer></HookUserReducer>*/}
+        <DocTitleOne></DocTitleOne>
     </div>
   );
 }
